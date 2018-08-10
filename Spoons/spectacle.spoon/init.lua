83a842f127887e225e4963bc2662edba44c56bae
Cameron Ruatta <cameron@ruatta.com>
2018-08-09 22:08:26 -0700

    Adding basic wm

---
+++ Spoons/spectacle.spoon/init.lua
@@ -0,0 +1,90 @@
+local obj = {}
+obj.__index = obj
+
+-- Metadata
+obj.name = "spectacle"
+obj.version = "0.0.20180809"
+obj.author = "Cameron Ruatta <c at ruatta.com>"
+obj.license = "MIT"
+obj.homepage = "https://github.com/cruatta/dotfiles-hammerspoon"
+
+--- spectacles:init()
+--- Method
+--- Start spectacles
+---
+--- Parameters:
+---  * None
+function obj:init()
+
+-- disable animation
+hs.window.animationDuration = 0
+
+hs.hotkey.bind({"alt", "ctrl"}, "Left", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
+end)
+
+hs.hotkey.bind({"alt", "ctrl"}, "Right", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x + (max.w / 2)
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
+end)
+
+hs.hotkey.bind({"alt", "ctrl"}, "Down", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y + (max.h / 2)
+    f.w = max.w
+    f.h = max.h / 2
+    win:setFrame(f)
+end)
+
+hs.hotkey.bind({"alt", "ctrl"}, "Up", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w
+    f.h = max.h / 2
+    win:setFrame(f)
+end)
+
+hs.hotkey.bind({"alt", "ctrl"}, "f", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w
+    f.h = max.h
+    win:setFrame(f)
+end)
+
+end
+
+return obj
+
